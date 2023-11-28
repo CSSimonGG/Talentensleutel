2f375b40576b6882780e751959efa943dcc4cd81
Max-Scholten <PS229429@summacollege.nl>
2023-11-28 16:43:08 +0100

    Made basic structure for New crud

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
-
 class ProfileController extends Controller
 {
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SleutelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(['auth'])->group(function () {
+// Profile Routes
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Songs Routes
+    Route::get('Talentensleutel/create', [SleutelController::class, 'create'])->name('Talentensleutel.create');
+    Route::get('Talentensleutel/{index}/edit', [SleutelController::class, 'edit'])->name('Talentensleutel.edit');
+    Route::put('Talentensleutel/{Talentensleutel}', [SleutelController::class, 'update'])->name('Talentensleutel.update');
+    Route::post('Talentensleutel', [SleutelController::class, 'store'])->name('Talentensleutel.store');
+    Route::delete('Talentensleutel/{Talentensleutel}', [SleutelController::class, 'destroy'])->name('Talentensleutel.destroy');
+
+
+}
+);
 require __DIR__.'/auth.php';
