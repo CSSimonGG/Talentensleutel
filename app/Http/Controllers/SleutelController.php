912dfd27cf12063e751d3d54bf4c20abfc21e91e
Max-Scholten <PS229429@summacollege.nl>
2023-11-29 12:48:08 +0100

    Made a new controller , modules and a request-auth and a seeder

--- app/Http/Controllers/SleutelController.php
+++ app/Http/Controllers/SleutelController.php
@@ -2,10 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Talentensleutel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SleutelController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Talentensleutel $talentensleute)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Talentensleutel $talentensleute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Talentensleutel $talentensleute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Talentensleutel $talentensleute)
+    {
+        //
+    }
 }
