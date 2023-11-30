a30335b5e13f61ecf93b858f73d24ae8ccd9a743
Max-Scholten <PS229429@summacollege.nl>
2023-11-30 12:58:35 +0100

    Added raport and reults page and added the web routes for it

---
+++ resources/views/talentsleutel/raporten.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot:title>
+        Raporten
+    </x-slot:title>
+    <div>
+        raporten
+    </div>
+</x-app-layout>
---
+++ resources/views/talentsleutel/resultaten.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot:title>
+        Resultaten
+    </x-slot:title>
+    <div>
+        resultaten
+    </div>
+</x-app-layout>
