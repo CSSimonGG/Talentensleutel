110f1a453820e356afbd69224c16f10ac7083eea
Max-Scholten <PS229429@summacollege.nl>
2023-11-30 10:40:56 +0100

    this shit should work (maybe?)

---
+++ resources/views/talentsleutel/home.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot:title>
+        Home
+    </x-slot:title>
+    <div>
+        test
+    </div>
+</x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
     },
     plugins: [
-        require('flowbite/plugin')
+
     ]
 
 }
