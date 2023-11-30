fcfccb15d336c3f7425f10dca085793467f8a180
Simon <98284824+CSSimonGG@users.noreply.github.com>
2023-11-30 22:10:12 +0100

    progress bar and buttons

--- resources/views/talentsleutel/questionnaire.blade.php
+++ resources/views/talentsleutel/questionnaire.blade.php
@@ -6,57 +6,7 @@
         <!-- Question Display -->
         <h2 class="p-4 font-bold text-xl">{{ $question->question }}</h2>
 
-        @if($question->id == '1' or $question->id == '8' or $question->id == '16' or $question->id == '18' or $question->id == '20' or $question->id == '24' or $question->id == '31' or $question->id == '36' or $question->id == '41' or $question->id == '47' or $question->id == '51' or $question->id == '57')
-            @if($question->id == '1') <!-- Start subject Algemene Gegevens -->
-
-            @elseif($question->id == '8') <!-- Start subject Bedrijfsinformatie -->
-
-            @elseif($question->id == '16') <!-- Start subject Missie -->
-
-            @elseif($question->id == '18') <!-- Start subject Heldere Focus -->
-            <div> <!-- Subject Information Focus -->
-                <div class="text-3xl object-center">
-                    <h1>
-                        <img src="/images/subjects/Focus.png" alt="" class="object-center h-96 ">
-                        HELDERE FOCUS
-                    </h1>
-                </div>
-                <br>
-                <p class="1 xl">
-                    Voordat je begint met het verbeteren van je leercultuur, is het belangrijk om je af te vragen wat je precies wilt bereiken.
-                    Denk hierbij aan doelen zoals het vergroten van de inzetbaarheid van je medewerkers, het verbeteren van hun prestaties, of het ontwikkelen van hun vaardigheden en talenten.
-                    Ook het optimaliseren van interne processen en het beter afstemmen van taken en inspanningen op de organisatiedoelstellingen kunnen doelstellingen zijn.
-                </p>
-            </div>
-            @elseif($question->id == '20') <!-- Start subject Mix aan Leermogelijkheden -->
-            <div> <!-- Subject Information Focus -->
-                <div class="text-3xl object-center">
-                    <h1>
-                        <img src="/images/subjects/" alt="" class="object-center h-96 ">
-                        Subject
-                    </h1>
-                </div>
-                <br>
-                <p class="1 xl">
-                    Subject description
-                </p>
-            </div>
-            @elseif($question->id == '24') <!-- Start subject Meerjarig Beleid -->
-
-            @elseif($question->id == '31') <!-- Start subject Inspirerend Leiderschap -->
-
-            @elseif($question->id == '36') <!-- Start subject Medewerker Centraal -->
-
-            @elseif($question->id == '41') <!-- Start subject Leervoorzieningen -->
-
-            @elseif($question->id == '47') <!-- Start subject Gezonde & Energieke Werkomgeving -->
-
-            @elseif($question->id == '51') <!-- Start subject Meetbare Leereffecten -->
-
-            @elseif($question->id == '57') <!-- Start subject Kennis -->
-
-            @endif
-        @elseif($question->question_type == '1')
+        @if($question->question_type == '1')
             <div> <!-- Question Type 0 (4 options) -->
                 <form id="questionForm" action="/questionnaire" method="POST">
                     @csrf
@@ -103,12 +53,19 @@
         @endif
 
         <!-- Navigation Buttons -->
-        <button >Previous</button>
-        <button type="button" onclick="submitForm()">Next</button>
+        <div class="absolute bottom-16 space-x-6 text-xl font-bold">
+            <button class="hover:bg-gray-200 p-2 rounded-full">Previous</button>
+            <span>{{ $question->id }}/62</span>
+            <button type="button" onclick="submitForm()">Next</button>
+        </div>
 
         <!-- Progress bar -->
-        <div class="w-full h-6 bg-gray-200 fixed bottom-5">
-            <div class="h-6 bg-Apple" style="width: 2%"></div>
+        <div class="h-6 bg-gray-200 fixed bottom-5 w-4/5">
+            @php
+                // Calculate the progress percentage
+                $progressPercentage = ($question->id / 62) * 100;
+            @endphp
+            <div class="h-6 bg-Apple" style="width: {{ $progressPercentage }}%"></div>
         </div>
 
         <script>
