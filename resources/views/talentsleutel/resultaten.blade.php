69dae97e0aa1dff2ffff2666ad45dce7d4596cae
Simon <98284824+CSSimonGG@users.noreply.github.com>
2023-12-01 02:18:30 +0100

    Hardcoded result page

--- resources/views/talentsleutel/resultaten.blade.php
+++ resources/views/talentsleutel/resultaten.blade.php
@@ -3,6 +3,88 @@
         Resultaten
     </x-slot:title>
     <div>
-        resultaten
+        <h1 class="text-2xl font-bold flex justify-center">Resultaten Bedrijf</h1>
+        <div>
+            <!-- Heldere Focus -->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Focus.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Heldere Focus</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-Apple rounded-full" style="width: 70%"></div>
+                </div>
+            </div>
+            <!-- Mix aan Leermogelijkheden -->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Leermix.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Mix aan Leermogelijkheden</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-Forestgreen rounded-full" style="width: 90%"></div>
+                </div>
+            </div>
+            <!-- Meerjarig Beleid -->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Meerjarig_beleid.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Meerjarig Beleid</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-YinMnBlue rounded-full" style="width: 40%"></div>
+                </div>
+            </div>
+            <!-- Inspirerend Leiderschap -->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Leiderschap.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Inspirerend Leiderschap</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-Apple rounded-full" style="width: 60%"></div>
+                </div>
+            </div>
+            <!-- Medewerker Centraal-->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Medewerker.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Medewerker Centraal</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-YinMnBlue rounded-full" style="width: 30%"></div>
+                </div>
+            </div>
+            <!-- Leervoorzieningen -->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Leervoorziening.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Leervoorzieningen</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-Apple rounded-full" style="width: 55%"></div>
+                </div>
+            </div>
+            <!-- Gezonde & Energieke Werkomgeving -->
+            <div class="h-[100px] w-[300px] flex">
+                <span class="flex break-normal">
+                    <img class="h-20" src="images/subjects/Werkomgeving.png">
+                    <h2 class="mt-3 ml-2 text-xl font-bold">Gezonde & Energieke Werkomgeving</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-Forestgreen rounded-full" style="width: 85%"></div>
+                </div>
+            </div>
+            <!-- Meetbare Leereffecten-->
+            <div class="h-[100px] flex">
+                <span class="flex">
+                    <img class="h-20" src="images/subjects/Leereffecten.png">
+                    <h2 class="mt-6 ml-2 text-xl font-bold">Meetbare Leereffecten</h2>
+                </span>
+                <div class="h-6 ml-10 mt-6 w-2/5 absolute left-1/3">
+                    <div class="h-6 bg-YinMnBlue rounded-full" style="width: 35%"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </x-app-layout>
