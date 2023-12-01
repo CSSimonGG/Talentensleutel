5449e2caddad06a35034ad61440c56ed935e097e
Max-Scholten <PS229429@summacollege.nl>
2023-12-01 09:42:52 +0100

    Contact and calendar page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="/images/TalentensleutelLogo-removebg-preview.png" alt="Test Image" class=" object-center   h-96 "/>
                 </a>
             </div>
 
--- resources/views/talentsleutel/contact.blade.php
+++ resources/views/talentsleutel/contact.blade.php
@@ -1,11 +1,46 @@
 <x-app-layout>
     <x-slot:title>
-        contact
+        Contact
     </x-slot:title>
-    <div>
-        <div class="text-3xl object-center">
-            cotact
+
+    <div class="container mx-auto p-8">
+        <div class="text-3xl text-center mb-8 font-bold">
+            Contactformulier
         </div>
 
+        <form action="/submit-contact-form" method="post">
+            <!-- Company Information -->
+            <label for="company-name">Bedrijfsnaam:</label>
+            <input type="text" id="company-name" name="company_name" required class="mb-4 p-2 w-full">
+
+            <!-- Contact Person Information -->
+            <label for="full-name">Naam en achternaam:</label>
+            <input type="text" id="full-name" name="full_name" required class="mb-4 p-2 w-full">
+
+            <!-- Sector Information -->
+            <label for="sector">Sector:</label>
+            <input type="text" id="sector" name="sector" required class="mb-4 p-2 w-full">
+
+            <!-- Email Address -->
+            <label for="email">E-mailadres:</label>
+            <input type="email" id="email" name="email" required class="mb-4 p-2 w-full">
+
+            <!-- Phone Number -->
+            <label for="phone">Telefoonnummer:</label>
+            <input type="tel" id="phone" name="phone" required class="mb-4 p-2 w-full">
+
+            <!-- Question/Comment -->
+            <label for="question-comment">Vraag/opmerking:</label>
+            <textarea id="question-comment" name="question_comment" required rows="4" class="mb-4 p-2 w-full"></textarea>
+
+            <!-- Privacy Statement -->
+            <label for="privacy-agreement" class="flex items-center">
+                <input type="checkbox" id="privacy-agreement" name="privacy_agreement" required class="mr-2">
+                Ik heb de privacyverklaring gelezen en ga hiermee akkoord
+            </label>
+
+            <!-- Submit Button -->
+            <button type="submit" class="bg-Forestgreen text-white p-2 rounded">Verstuur</button>
+        </form>
     </div>
 </x-app-layout>
